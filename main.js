f248e2722ae51a0e405c11caa957fca4ff15cc39
Azlankhan12345 <161835064+Azlankhan12345@users.noreply.github.com>
2024-03-29 00:27:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let barish_hoti_ha = false;
+let barish_Nhi_Hoti_Ha = true;
+let Too_Kiya_Hota_Ha;
+Too_Kiya_Hota_Ha = (barish_hoti_ha ? "Ziyada_Pani_Ata_Ha" : "Thora_Pani_Ata_Ha") || (barish_Nhi_Hoti_Ha ? "Garmi_Nhi_Hoti_Ha" : "Garm_Hoti_Ha");
+console.log(Too_Kiya_Hota_Ha);
+export {};
